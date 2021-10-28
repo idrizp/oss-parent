5fa4db99deb388b025afad59f036eaed61e7bccb
idrizp <idrizpelaj@hotmail.com>
2021-10-28 19:43:49 +0200

    Instead check for anything other than PICKUP

--- menu/src/main/java/me/idriz/oss/menu/listener/MenuListener.java
+++ menu/src/main/java/me/idriz/oss/menu/listener/MenuListener.java
@@ -3,6 +3,7 @@ package me.idriz.oss.menu.listener;
 
 import me.idriz.oss.menu.Menu;
 import me.idriz.oss.menu.MenuItem;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,40 +11,45 @@ import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.PlayerInventory;
 
 public class MenuListener implements Listener {
 
-    @EventHandler
-    public void onClose(InventoryCloseEvent e) {
-        Menu.MENUS.remove((Player) e.getPlayer());
-    }
+	@EventHandler
+	public void onClose(InventoryCloseEvent e) {
+		Menu.MENUS.remove((Player) e.getPlayer());
+	}
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent e) {
-        Menu.MENUS.remove(e.getPlayer());
-    }
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e) {
+		Menu.MENUS.remove(e.getPlayer());
+	}
 
-    @EventHandler
-    public void onClick(InventoryClickEvent e) {
-        if(Menu.MENUS.containsKey((Player) e.getWhoClicked())) {
-            Player player = (Player) e.getWhoClicked();
-            Menu menu = Menu.MENUS.get(player);
+	@EventHandler
+	public void onClick(InventoryClickEvent e) {
+		if (Menu.MENUS.containsKey((Player) e.getWhoClicked())) {
+			Player player = (Player) e.getWhoClicked();
+			Menu menu = Menu.MENUS.get(player);
 
-            if(menu.getItems().containsKey(e.getRawSlot())) {
-                MenuItem item = menu.getItems().get(e.getRawSlot());
-                if(item == null) {
-                    if (!e.getAction().name().startsWith("PLACE")) {
-                        return;
-                    }
-                    menu.onItemMove(e);
+			MenuItem item = menu.getItems().get(e.getRawSlot());
+			if (item == null) {
+                if (e.getClickedInventory() instanceof PlayerInventory) {
                     return;
                 }
-                if(item.isCancel()) {
-                    e.setCancelled(true);
-                }
-                item.getClickConsumer().accept(player);
-            }
-        }
-    }
+				if (e.getCursor() == null) {
+					return;
+				}
+				if (e.getAction().name().startsWith("PICKUP")) {
+					return;
+				}
+				menu.onItemMove(e);
+				return;
+			}
+			if (item.isCancel()) {
+				e.setCancelled(true);
+			}
+			item.getClickConsumer().accept(player);
+		}
+	}
 
 }
